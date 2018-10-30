714f5d8cab832d5eeafa10151d501bce88f8dd6b
mytv100 <mytv7609@gmail.com>
2018-10-30 16:52:27 +0900

    init2

--- Eris_backend_project/configuration/urls.py
+++ Eris_backend_project/configuration/urls.py
@@ -1,4 +1,4 @@
-"""Eris_backend_project URL Configuration
+"""configuration URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
