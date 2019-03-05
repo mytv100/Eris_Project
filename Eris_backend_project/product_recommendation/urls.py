1d1a7ecb468046391abd78046e5cbe9db94668f9
mytv100 <2qudgusals>
2019-03-05 23:11:55 +0900

    주석 삭제 2019/03/05

--- Eris_backend_project/product_recommendation/urls.py
+++ Eris_backend_project/product_recommendation/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from product_recommendation import views
 
 urlpatterns = [
-    # view 를 여기에 등록
     path('asdf/', views.AsdfAPIView.as_view()),
     path('lbh/', views.LBHAPIView.as_view()),
 
