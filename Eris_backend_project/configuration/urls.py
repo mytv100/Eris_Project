9badbfe8ddf08629f97335d1c377e0f73334f71f
mytv100 <mytv7609@gmail.com>
2019-03-24 22:57:43 +0900

    url comment update

--- Eris_backend_project/configuration/urls.py
+++ Eris_backend_project/configuration/urls.py
@@ -21,9 +21,9 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="불화의 여신 Eris와 함께하는 신나는 졸업작품 프로젝트",
+        title="졸업작품 프로젝트",
         default_version='version Alpha 0.0.1',
-        description="문서 상세 설명", #문서 상세 설명이라는게 이 프로젝트에 대한? 이 urls.py에 대한?
+        description="문서 상세 설명",
         terms_of_service="https://github.com/mytv100/Eris_Project",
         # 이메일 형식 안맞으면 internal error 발생함
         contact=openapi.Contact(email="mytv7609@gmail.com"),
@@ -38,14 +38,14 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    #react without ui & only data
-    #path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # react without ui & only data
+    # path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('api/doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui', ),
     path('api/doc/redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    path('product-recommend/', include('movie_recommendation.urls'), name='product-recommend-app'),
-    #rest_framework Authentication
-    path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-
+    path('movie-recommend/', include('movie_recommendation.urls'), name='product-recommend-app'),
+    # rest_framework Authentication
+    path(r'api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
 ]
+
