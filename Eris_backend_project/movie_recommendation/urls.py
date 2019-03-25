acc5639847507b4a833c6d0fb30b6ed8edac54aa
mytv100 <mytv7609@gmail.com>
2019-03-25 19:46:47 +0900

    add urlpattenrs and comments

--- Eris_backend_project/movie_recommendation/urls.py
+++ Eris_backend_project/movie_recommendation/urls.py
@@ -4,12 +4,28 @@ from rest_framework import routers
 from movie_recommendation import views
 
 router = routers.SimpleRouter()
-router.register(r'movie', views.MovieAPIViewSet)
+
+# 고객 - 영화 M2M
+# 각 고객의 각 영화 평점 /customerMovie/
+# 각 고객에 대한 영화 추천 리스트 /customerMovie/movie_list/
+router.register(r'customerMovie', views.CustomerMovieAPIViewSet)
+
+# 업체 - 영화 M2M
+# 각 업체별 소유한 영화 /businessPartnerMovie
+router.register(r'businessPartnerMovie', views.BusinessPartnerMovieAPIViewSet)
+
+# 배우 - 영화
+# 영화에 출연하는 배우들 /actorMovie/
+router.register(r'actorMovie', views.ActorMovieAPIViewSet)
+
+# 고객
+# 업체의 고객 정보 /customer/
+router.register(r'customer', views.CustomerAPIViewSet)
+
 
 urlpatterns = [
-
-    path('lbh/', views.LBHAPIView.as_view()),
-
+    # 회원가입 -> 바로 로그인되서 /api/doc/로 넘어감
+    path('join/', views.signup, name='join'),
 ]
 
 urlpatterns += router.urls
