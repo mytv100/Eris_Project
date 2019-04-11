48d0d479e695d4e93f641d701f71362210b1694a
mytv100 <mytv7609@gmail.com>
2019-04-11 14:46:12 +0900

    Add comments

--- Eris_backend_project/movie_recommendation/urls.py
+++ Eris_backend_project/movie_recommendation/urls.py
@@ -6,26 +6,26 @@ from movie_recommendation import views
 router = routers.SimpleRouter()
 
 # 고객 - 영화 M2M
-# 각 고객의 각 영화 평점 /customerMovie/
+# 각 고객의 각 영화 평점 /customerMovie/     permission = IsAuthenticated
 # 각 고객에 대한 영화 추천 리스트 /customerMovie/movie_list/
 router.register(r'customerMovie', views.CustomerMovieAPIViewSet)
 
 # 업체 - 영화 M2M
-# 각 업체별 소유한 영화 /businessPartnerMovie
+# 각 업체별 소유한 영화 /businessPartnerMovie        permission = IsAdmin
 router.register(r'businessPartnerMovie', views.BusinessPartnerMovieAPIViewSet)
 
-# 배우 - 영화
-# 영화에 출연하는 배우들 /actorMovie/
+# 배우 - 영화 M2M
+# 영화에 출연하는 배우들 /actorMovie/     permission = IsAdmin
 router.register(r'actorMovie', views.ActorMovieAPIViewSet)
 
 # 고객
-# 업체의 고객 정보 /customer/
+# 업체의 고객 정보 /customer/      permission = IsAuthentication
 router.register(r'customer', views.CustomerAPIViewSet)
 
-# 업체 회원 가입 /signup/
+# 업체 회원 가입 /signup/     permission = Anyone allowed without authentication
 router.register(r'signup', views.CreateBusinessPartnerAPIViewSet)
 
-# 데이터 초기화
+# 데이터 초기화 /init/        permission = IsAdmin
 router.register(r'init', views.InitViewSet)
 
 urlpatterns = [
