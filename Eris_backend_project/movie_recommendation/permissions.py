443399b3432c1c23b1c705e0bf72bb233770f5b3
mytv100 <mytv7609@gmail.com>
2019-04-11 15:09:46 +0900

    Add comments

--- Eris_backend_project/movie_recommendation/permissions.py
+++ Eris_backend_project/movie_recommendation/permissions.py
@@ -3,14 +3,14 @@ from rest_framework import permissions
 
 class UserPermission(permissions.BasePermission):
 
-    # before signin
+    # before SignIn
     def has_permission(self, request, view):
         if not request.user.is_authenticated or request.user.is_superuser:
             return True
         else:
             return False
 
-    # after signup( if user is authenticated, user can't see api that it applied
+    # after SignUp( if user is authenticated, user can't see the API to which this permission class applies)
     def has_object_permission(self, request, view, obj):
         if request.user.is_authenticated:
             return False
