95a04b5363f0336e12c63715d243301236253e81
mytv100 <mytv7609@gmail.com>
2019-04-30 11:52:00 +0900

    remove unused method

--- Eris_backend_project/movie_recommendation/permissions.py
+++ Eris_backend_project/movie_recommendation/permissions.py
@@ -3,16 +3,14 @@ from rest_framework import permissions
 
 class UserPermission(permissions.BasePermission):
 
-    # before SignIn
+    # after SignUp( if user is authenticated, user can't see the API to which this permission class applies)
     def has_permission(self, request, view):
-        if not request.user.is_authenticated or request.user.is_superuser:
+        if not (request.user.is_authenticated or request.user.is_superuser):
             return True
         else:
             return False
 
-    # after SignUp( if user is authenticated, user can't see the API to which this permission class applies)
-    def has_object_permission(self, request, view, obj):
-        if request.user.is_authenticated:
-            return False
-        else:
-            return True
+
+    """
+    회원가입 탈퇴 정보변경 같은거 로그인 상태에 따라, request.method의 종류에 따라
+    """
