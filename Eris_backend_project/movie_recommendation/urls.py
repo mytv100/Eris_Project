6287de3d2259f84b4345c9903b054e8c823e7f4c
mytv100 <mytv7609@gmail.com>
2019-04-30 11:52:23 +0900

    add custom router in urls.py

--- Eris_backend_project/movie_recommendation/urls.py
+++ Eris_backend_project/movie_recommendation/urls.py
@@ -2,32 +2,38 @@ from django.urls import path
 from rest_framework import routers
 
 from movie_recommendation import views
+from movie_recommendation.router import router
 
-router = routers.SimpleRouter()
-
+simple_router = routers.SimpleRouter()
+movie_title_router = router.CustomMovieTitleRouter()
+movie_title_nickname_router = router.CustomMovieTitleNicknameRouter()
 # 고객 - 영화 M2M
 # 각 고객의 각 영화 평점 /customerMovie/     permission = IsAuthenticated
 # 각 고객에 대한 영화 추천 리스트 /customerMovie/movie_list/
-router.register(r'customerMovie', views.CustomerMovieAPIViewSet)
+movie_title_nickname_router.register(r'customerMovie', views.CustomerMovieAPIViewSet)
 
 # 업체 - 영화 M2M
 # 각 업체별 소유한 영화 /businessPartnerMovie        permission = IsAdmin
-router.register(r'businessPartnerMovie', views.BusinessPartnerMovieAPIViewSet)
+movie_title_router.register(r'businessPartnerMovie', views.BusinessPartnerMovieAPIViewSet)
 # 배우 - 영화 M2M
 # 영화에 출연하는 배우들 /actorMovie/     permission = IsAdmin
-router.register(r'actorMovie', views.ActorMovieAPIViewSet)
+simple_router.register(r'actorMovie', views.ActorMovieAPIViewSet)
 
 # 고객
 # 업체의 고객 정보 /customer/      permission = IsAuthentication
-router.register(r'customer', views.CustomerAPIViewSet)
+simple_router.register(r'customer', views.CustomerAPIViewSet)
 
 # 업체 회원 가입 /signup/     permission = Anyone allowed without authentication
-router.register(r'signup', views.CreateBusinessPartnerAPIViewSet)
+simple_router.register(r'signup', views.CreateBusinessPartnerAPIViewSet)
 
 # 데이터 초기화 /init/        permission = IsAdmin
-router.register(r'init', views.InitViewSet)
+# simple_router.register(r'init', views.InitViewSet)
 
 urlpatterns = [
 ]
 
-urlpatterns += router.urls
+urlpatterns += simple_router.urls
+urlpatterns += movie_title_router.urls
+urlpatterns += movie_title_nickname_router.urls
+
+
