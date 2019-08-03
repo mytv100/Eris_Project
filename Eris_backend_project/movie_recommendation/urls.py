ae92e14004ac3ee943d49783c3671cd673ed3682
mytv100 <mytv7609@gmail.com>
2019-08-03 09:26:29 +0900

    change urls.py

--- Eris_backend_project/movie_recommendation/urls.py
+++ Eris_backend_project/movie_recommendation/urls.py
@@ -5,23 +5,28 @@ from movie_recommendation import views
 from movie_recommendation.router import router
 
 simple_router = routers.SimpleRouter()
-movie_title_router = router.CustomMovieTitleRouter()
-movie_title_nickname_router = router.CustomMovieTitleNicknameRouter()
+
+# movie_title_router = router.CustomMovieTitleRouter()
+# movie_title_nickname_router = router.CustomMovieTitleNicknameRouter()
+
 # 고객 - 영화 M2M
 # 각 고객의 각 영화 평점 /customerMovie/     permission = IsAuthenticated
 # 각 고객에 대한 영화 추천 리스트 /customerMovie/movie_list/
-movie_title_nickname_router.register(r'customerMovie', views.CustomerMovieAPIViewSet)
+# movie_title_nickname_router.register(r'customerMovie', views.CustomerMovieAPIViewSet)
 
 # 업체 - 영화 M2M
 # 각 업체별 소유한 영화 /businessPartnerMovie        permission = IsAdmin
-movie_title_router.register(r'businessPartnerMovie', views.BusinessPartnerMovieAPIViewSet)
+# movie_title_router.register(r'businessPartnerMovie', views.BusinessPartnerMovieAPIViewSet)
 # 배우 - 영화 M2M
 # 영화에 출연하는 배우들 /actorMovie/     permission = IsAdmin
-simple_router.register(r'actorMovie', views.ActorMovieAPIViewSet)
+#simple_router.register(r'actorMovie', views.ActorMovieAPIViewSet)
 
 # 고객
 # 업체의 고객 정보 /customer/      permission = IsAuthentication
-simple_router.register(r'customer', views.CustomerAPIViewSet)
+#simple_router.register(r'customer', views.CustomerAPIViewSet)
+simple_router.register(r'customer', views.NewCustomerAPIViewSet)
+
+simple_router.register(r'movie', views.NewMovieAPIViewSet)
 
 # 업체 회원 가입 /signup/     permission = Anyone allowed without authentication
 simple_router.register(r'signup', views.CreateBusinessPartnerAPIViewSet)
@@ -33,7 +38,7 @@ urlpatterns = [
 ]
 
 urlpatterns += simple_router.urls
-urlpatterns += movie_title_router.urls
-urlpatterns += movie_title_nickname_router.urls
+# urlpatterns += movie_title_router.urls
+# urlpatterns += movie_title_nickname_router.urls
 
 
