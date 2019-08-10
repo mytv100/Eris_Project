62cc77ce9879b93de8e894ae0abfa0acd701a41b
mytv100 <mytv7609@gmail.com>
2019-08-11 01:53:22 +0900

    add url about movie recommendation

--- Eris_backend_project/movie_recommendation/urls.py
+++ Eris_backend_project/movie_recommendation/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.urls import path, include
 from rest_framework import routers
 
@@ -7,11 +8,11 @@ simple_router = routers.SimpleRouter()
 default_router = routers.DefaultRouter()
 
 default_router.register(r'movie', views.MovieAPIViewSet)
+default_router.register(r'customer', views.CustomerAPIViewSet)
 
 urlpatterns = [
-    # path('', views.initData),
-
-
+    # path('init', views.initData),
+    url(r'^recommend/(?P<customer_id>\d+)/(?P<movie_id>\d+)/?', views.MovieRecommend),
 ]
 
 urlpatterns += simple_router.urls
